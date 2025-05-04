04b17eb49836858b676a23dca7e1c3339576f2df
Mostofa <mostofacba@gmail.com>
2025-05-04 16:26:07 +0600

    Add project files.

---
+++ FunnyJokesApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FunnyJokesApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
