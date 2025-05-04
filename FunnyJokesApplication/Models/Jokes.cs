cf057c139219a77ffb05557e28f22466e4ed6c58
Mostofa <mostofacba@gmail.com>
2025-05-04 17:03:18 +0600

    Jokes Model Updates

--- FunnyJokesApplication/Models/Jokes.cs
+++ FunnyJokesApplication/Models/Jokes.cs
@@ -3,6 +3,14 @@ namespace FunnyJokesApplication.Models
 {
     public class Jokes
     {
-        public int MyProperty { get; set; }
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+
+        public Jokes()
+        {
+
+        }
     }
 }
