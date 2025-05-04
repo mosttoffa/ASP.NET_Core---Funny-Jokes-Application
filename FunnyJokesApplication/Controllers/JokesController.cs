5525f694438b1001ec29f6d1e5e352a482a077b2
Mostofa <mostofacba@gmail.com>
2025-05-04 19:03:26 +0600

    Create Jokes and Search view

--- FunnyJokesApplication/Controllers/JokesController.cs
+++ FunnyJokesApplication/Controllers/JokesController.cs
@@ -25,6 +25,12 @@ namespace FunnyJokesApplication.Controllers
             return View(await _context.Jokes.ToListAsync());
         }
 
+        // GET: Jokes/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
